6ba25654cef68d331e2f12cb887e7fafb52951cd
Christerpher Hunter <djhunter67@gmail.com>
2022-08-30 13:13:00 -0400

    initial template setup complete and functional; base.html setup, template inheritance functional

--- site/main.py
+++ site/main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -5,41 +6,36 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="site/static"), name="static")
+ROOT = Path(__file__).parent
 
-template = Jinja2Templates(directory="site/templates")
+templates = Jinja2Templates(directory=str(ROOT / "templates"))
 
+app.mount("/static", StaticFiles(directory=str(ROOT / "static")), name="static")
 
 @app.get("/")
-async def root():
-    return HTMLResponse(content=template.render("index.html"))
-
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/about")
-async def about():
-    return HTMLResponse(content=template.render("about.html"))
-
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
 
 @app.get("/meme")
-async def meme():
-    return HTMLResponse(content=template.render("meme.html"))
-
+async def meme(request: Request):
+    return templates.TemplateResponse("meme.html", {"request": request})
 
 @app.get("/meme/{meme_id}")
-async def meme_id(meme_id: int):
-    return HTMLResponse(content=template.render("meme.html"))
-
+async def meme_id(request: Request, meme_id: int):
+    return templates.TemplateResponse("meme.html", {"request": request, "meme_id": meme_id})
 
 @app.get("/stock")
-async def stock():
-    return HTMLResponse(content=template.render("stock.html"))
-
+async def stock(request: Request):
+    return templates.TemplateResponse("stock.html", {"request": request})
 
 @app.get("/fact")
-async def fact():
-    return HTMLResponse(content=template.render("fact.html"))
-
+async def fact(request: Request):
+    return templates.TemplateResponse("fact.html", {"request": request})
 
 @app.get("/fact/{topic}", response_class=HTMLResponse)
 async def fact_topic(request: Request, topic: str):
-    return template.TemplateResponse("fact.html", {"request": request, "topic": topic})
+    return templates.TemplateResponse("fact.html", {"request": request, "topic": topic})
