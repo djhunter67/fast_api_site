40eaf7bca2d246a7af871b5c7d80cfcb512809af
Christerpher Hunter <djhunter67@gmail.com>
2022-09-07 11:45:56 -0400

    removed the login/register button; added contact button and form

--- main.py
+++ main.py
@@ -23,17 +23,11 @@ token_auth_scheme = HTTPBearer()
 async def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
-@app.get("/login")
-async def login(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
 
-@app.get("/register")
-async def register(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
+@app.get("/contact")
+async def contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request})
 
-@app.get("/logout")
-async def logout(request: Request):
-    return templates.TemplateResponse("logout.html", {"request": request})
 
 @app.get("/personal")
 def private(request: Request, token: str = Depends(token_auth_scheme)):
@@ -46,25 +40,31 @@ def private(request: Request, token: str = Depends(token_auth_scheme)):
 
     return result
 
+
 @app.get("/about")
 async def about(request: Request):
     return templates.TemplateResponse("about.html", {"request": request})
+
 
 @app.get("/meme")
 async def meme(request: Request):
     return templates.TemplateResponse("meme.html", {"request": request})
 
+
 @app.get("/meme/{meme_id}")
 async def meme_id(request: Request, meme_id: int):
     return templates.TemplateResponse("meme.html", {"request": request, "meme_id": meme_id})
+
 
 @app.get("/stock")
 async def stock(request: Request):
     return templates.TemplateResponse("stock.html", {"request": request})
 
+
 @app.get("/fact")
 async def fact(request: Request):
     return templates.TemplateResponse("fact.html", {"request": request})
+
 
 @app.get("/fact/{topic}", response_class=HTMLResponse)
 async def fact_topic(request: Request, topic: str):
